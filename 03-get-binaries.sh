097f721fbd74043052521c7c2848992890cfc89b
zhangguanzhang <zhangguanzhang@qq.com>
2020-07-24 16:58:36 +0800

    change etcd back to v3.3.22,because https://github.com/etcd-io/etcd/issues/12156

--- 03-get-binaries.sh
+++ 03-get-binaries.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-:  ${KUBE_VERSION:=v1.16.13} ${ETCD_version:=v3.3.23} ${FLANNEL_version:=v0.11.0}
+:  ${KUBE_VERSION:=v1.16.13} ${ETCD_version:=v3.3.22} ${FLANNEL_version:=v0.11.0}
 :  ${OS:=linux} ${ARCH:=amd64} ${CNI_VERSION:=v0.8.3} #https://github.com/zhangguanzhang/k8s_bin-docker_cp/tree/develop/sync/cni_plugins/tag
 :  ${CNI_URL:=https://github.com/containernetworking/plugins/releases/download}
 
